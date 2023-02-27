ee0b7af5c121ce9ef5401c0d678c1e7c4786f6dd
citysushi89 <owenpickard89@gmail.com>
2023-02-27 12:25:37 -0500

    change theme

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
         appBar: AppBar(
